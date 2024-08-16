24c83d59bb2a8aae51771042cf0670419cd33abe
Melissa-sis112 <ibemaguzmeli.17@gmail.com>
2024-08-16 18:00:37 -0400

    v3

--- Ej1-Calculadora.py
+++ Ej1-Calculadora.py
@@ -1,51 +1,55 @@
 
-contador = 0
-print("--- CALCULADORA ---")
-while contador != 6:
-    print("1. Suma")
-    print("2. Resta")
-    print("3. Múltiplicación")
-    print("4. División")
-    print("5. Salir")
-    elegir = int(input("Elija una opción: "))
-    if elegir == 1:
-        numero1= int(input("Ingrese el primer número a evaluar: "))
-        numero2= int(input("Ingrese el segundo número a evaluar: "))
-        suma = numero1 + numero2
-        print("La suma es: " + str (suma))
-    elif elegir == 2:
-        numero1= int(input("Ingrese el primer número a evaluar: "))
-        numero2= int(input("Ingrese el segundo número a evaluar: "))
-        resta = numero1 - numero2
-        print("La resta es: "+ str(resta))
-    elif elegir == 3:
-        numero1= int(input("Ingrese el primer número a evaluar: "))
-        numero2= int(input("Ingrese el segundo número a evaluar: "))
-        multi= numero1 * numero2
-        print("La multiplicación es: "+str(multi))
-    elif elegir == 4:
-        numero1= int(input("Ingrese el primer número a evaluar: "))
-        numero2= int(input("Ingrese el segundo número a evaluar: "))
-        divi= numero1/numero2 
-        print("La división es: "+str(divi))
-    else:
-        print("--- Eso es todo :/, Gracias ---")
+def suma(numero1, numero2):
+    return numero1 + numero2
+def resta(numero1, numero2):
+    return numero1 - numero2
+def multiplicacion (numero1, numero2):
+    return numero1 * numero2
+def division(numero1, numero2):
+    if numero2 == 0:
+        return ("No existe división entre 0")
+    return numero1 / numero2
+def calculadora():
+    contador = 0
+    print ("--- CAlCULADORA ---")
+    while contador != 6:
+        print("1. Suma")
+        print("2. Resta")
+        print("3. Múltiplicación")
+        print("4. División")
+        print("5. Salir")
+        elegir = int(input("Elija una opción: "))
+        if elegir == 1:
+            numero1 = int(input("Ingrese el primer número a evalular: "))
+            numero2 = int(input("Ingrese el segundo número a evalular: "))
+            resultado = suma(numero1, numero2)
+            print("La suma es: " + str(resultado))
+        elif elegir == 2:
+            numero1 = int(input("Ingrese el primer número a evalular: "))
+            numero2 = int(input("Ingrese el segundo número a evalular: "))
+            resultado = resta(numero1, numero2)
+            print("La resta es: " + str(resultado))
+        elif elegir == 3:
+            numero1 = int(input("Ingrese el primer número a evalular: "))
+            numero2 = int(input("Ingrese el segundo número a evalular: "))
+            resultado = multiplicacion(numero1, numero2)
+            print("La multiplicación es: " + str(resultado))
+        elif elegir == 4:
+            numero1 = int(input("Ingrese el primer número a evalular: "))
+            numero2 = int(input("Ingrese el segundo número a evalular: "))
+            resultado = division(numero1, numero2)
+            print("La división es: " + str(resultado))
+        elif elegir == 5:
+            print ("--- Eso es todo :/, Gracias ---")
+            break
+        else:
+            print(">>> Por favor elija de nuevo <<<")
+calculadora ()
 
-    #FUNCIONES 
-elegir = input("Elija una operación: ")
-def suma (a,b):
-    return a+b
 
-def resta (a,b):
-    return a-b
 
-def multi (a,b):
-    return a*b
-    
-def divi (a,b):
-    return a/b
-a = int(input("Ingrese el primer número: "))
-b = int(input("Ingrese el segundo número: "))
+
+
 
 
 
