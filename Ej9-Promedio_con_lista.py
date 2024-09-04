ff3118b1966e3381d089b5651de15a8767354424
Melissa-sis112 <ibemaguzmeli.17@gmail.com>
2024-09-04 17:23:41 -0400

    listas

--- Ej9-Promedio_con_lista.py
+++ Ej9-Promedio_con_lista.py
@@ -3,11 +3,11 @@ promedio = 0
 cantidad = int(input("Ingrese la cantidad de estudiantes a evaluar: "))
 notas = []
 
-for i in range(cantidad):
+for i in range(cantidad): #en el bucle, de acuerdo a la cantidad se piden las notas 
     nota = int(input("Ingrese la nota del estudiante: "))
-    notas.append(nota)
+    notas.append(nota) #se agrega las notas ingresadas a la lista 
 
-suma = sum(notas)
+suma = sum(notas) #se suman las notas para luego sacar el promedio 
 promedio = suma / cantidad
 
 print("Las notas son:", notas)
