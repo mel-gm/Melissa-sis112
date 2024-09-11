d5eccab9c35e3e20763b9a2ed7c1add294bad942
Melissa-sis112 <ibemaguzmeli.17@gmail.com>
2024-09-11 19:19:30 -0400

    Funciones Convertidoruni

--- Ej9-Promedio_con_lista.py
+++ Ej9-Promedio_con_lista.py
@@ -4,8 +4,10 @@ cantidad = int(input("Ingrese la cantidad de estudiantes a evaluar: "))
 notas = []
 
 for i in range(cantidad): #en el bucle, de acuerdo a la cantidad se piden las notas 
-    nota = int(input("Ingrese la nota del estudiante: "))
-    notas.append(nota) #se agrega las notas ingresadas a la lista 
+    numNotas = int(input("Ingrese la cantidad de notas: "))
+    for q in range(numNotas):
+        nota = int(input("Ingrese la nota del estudiante: "))
+        notas.append(nota) #se agrega las notas ingresadas a la lista 
 
 suma = sum(notas) #se suman las notas para luego sacar el promedio 
 promedio = suma / cantidad
@@ -13,6 +15,7 @@ promedio = suma / cantidad
 print("Las notas son:", notas)
 print("La suma de las notas es:", suma)
 print("El promedio de las notas es:", promedio)
+#deberia generar una lista para cada estudiante y un promedio para cada estudiante 
 
 
 
